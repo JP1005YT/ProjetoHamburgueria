dd895621e9c30c688fd953f194e26b9ce721e1e9
JP1005YT <godlolpro32@gmail.com>
2024-07-27 18:12:50 -0300

    "pagina clientes pronta"

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const { createRecord, readRecords, updateRecord, deleteRecord } = require("./modules/sqliteManager")
 const app = express()
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
 
 app.listen("3000",()=>{
     console.log("http://localhost:3000/")
@@ -19,4 +21,25 @@ app.get("/getProdutos",async (req,res) =>{
 app.get("/getPedidos",async (req,res) =>{
     const resp = await readRecords('pedidos')
     res.send(resp)
+})
+
+app.post("/editClient",async (req,res) => {
+    const clientData = req.body
+    const { id, ...dataWithoutId } = clientData; // Remove o id do objeto
+    try{
+        await updateRecord("clientes",clientData.id,clientData)
+        res.status(200).send({error : false})
+    }catch{
+        res.status(402).send({error : true})
+    }
+})
+
+app.post("/delClient",async (req,res) => {
+    const {id} = req.body
+    try{
+        await deleteRecord('clientes',id)
+        res.status(200).send({error : false})
+    }catch{
+        res.status(402).send({error : true})
+    }
 })
